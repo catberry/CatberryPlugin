3deebe105acf8f83c6bb54983d6e60f984abd826
Alex Prokofiev <prokofiev.ad@yandex.ru>
2016-03-09 23:08:04 +0300

    basic component reference

---
+++ src/org/buffagon/intellij/catberry/CatberryComponentReference.java
@@ -0,0 +1,35 @@
+package org.buffagon.intellij.catberry;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReferenceBase;
+import com.intellij.psi.html.HtmlTag;
+import com.intellij.psi.xml.XmlTag;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+
+/**
+ * @author Prokofiev Alex
+ */
+public class CatberryComponentReference extends PsiReferenceBase<XmlTag>{
+  private final String key;
+  public CatberryComponentReference(HtmlTag element, String key) {
+    super(element);
+    this.key = key;
+  }
+
+  @Nullable
+  @Override
+  public PsiElement resolve() {
+    Project project = myElement.getProject();
+
+    return null;
+  }
+
+  @NotNull
+  @Override
+  public Object[] getVariants() {
+    return new Object[0];
+  }
+}
