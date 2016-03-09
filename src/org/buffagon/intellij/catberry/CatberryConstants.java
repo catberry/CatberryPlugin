66c3d75cf9cdb5721e9b47a949939cff827942e7
Alex Prokofiev <prokofiev.ad@yandex.ru>
2016-03-09 23:18:53 +0300

    extracts constants

--- src/org/buffagon/intellij/catberry/CatberryConstants.java
+++ src/org/buffagon/intellij/catberry/CatberryConstants.java
@@ -5,6 +5,8 @@ package org.buffagon.intellij.catberry;
  */
 public final class CatberryConstants {
   public static final String DEFAULT_MODULE_JS = "index.js";
+  public static final String DEFAULT_TEMPLATE_ENGINE = "handlebars";
   public static final String CAT_COMPONENT_JSON = "cat-component.json";
-
+  public static final String CATBERRY_COMPONENTS = "catberry_components";
+  public static final String CATBERRY_STORES = "catberry_stores";
 }
